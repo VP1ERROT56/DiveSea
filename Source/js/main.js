28f1b7f80dc8a7ee365816442734f961e8feacf8
VP1ERROT56 <vova.zhyravchenko.46@gmail.com>
2025-02-09 21:39:51 +0300

    navbar finished

--- Source/js/main.js
+++ Source/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("burgers").addEventListener("click", function () {
-        document.querySelector("nav").classList.toggle("open")
+        document.getElementById("nav__item").classList.toggle("open")
     })
 })
